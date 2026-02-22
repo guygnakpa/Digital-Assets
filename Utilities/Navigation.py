e019ad7f9a8e8113053ff5e2700d747a223d84cb
Guy Gnakpa <guygnakpa@gmail.com>
2026-02-21 21:54:14 -0500

    Update Navigation.py

--- Utilities/Navigation.py
+++ Utilities/Navigation.py
@@ -15,10 +15,12 @@
 # import webbrowser
 #import openpyxl as xls
 #import datetime
+import streamlit as st
+from streamlit_option_menu import option_menu
 #########################################################
 # The logic below will hide the streamlit auto page menu as it is redundant
 def hide_streamlit_nav():
-    import streamlit as st
+    #import streamlit as st
     st.markdown("""
     <style>
         [data-testid="stSidebarNav"] {display: none;}
@@ -56,8 +58,8 @@ ICONS = {
 }
 
 def render_sidebar(current_label: str) -> None:
-    import streamlit as st
-    from streamlit_option_menu import option_menu
+    #import streamlit as st
+    #from streamlit_option_menu import option_menu
     """Renders sidebar option_menu and navigates to selected page."""
     labels = list(PAGES.keys())
     icons = [ICONS.get(lbl, "circle") for lbl in labels]
